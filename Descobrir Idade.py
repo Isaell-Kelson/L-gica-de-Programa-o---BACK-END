dd4b682cd5cd71aeb8c93754a5c0c4911d91a7fe
Isaell Kelson <113737085+Isaell-Kelson@users.noreply.github.com>
2022-09-30 11:34:32 -0300

    Add files via upload

---
+++ Descobrir Idade.py
@@ -0,0 +1,32 @@
+print('          ---- Insira seu nome e data de nascimento para sabermos sua idade ----')
+print('')
+rodar = True
+while(rodar==True):
+
+    try:
+        print('Digite o seu nome: ')
+        
+    
+        nome = str(input())
+        print('')
+
+        print('Ano de nascimento: ')
+        
+
+        nasc = int(input())
+        print('')
+
+        
+        ano2 = 2022
+
+        if nasc >= 1922 and nasc <= 2022:
+            res = ano2 - nasc
+            print('Você é: ',nome)
+            print('Sua idade é: ',res)
+
+        rodar = False
+
+    except:
+        print('Dados incorretos')
+
+print('Finalizado')
