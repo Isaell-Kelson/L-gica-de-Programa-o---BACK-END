a846cbbb4ef26316377db1012f88c6133c0f0fdb
Isaell Kelson <113737085+Isaell-Kelson@users.noreply.github.com>
2022-10-21 11:46:54 -0300

    Add files via upload

---
+++ String.py
@@ -0,0 +1,15 @@
+print('Vamos testar três manipulações de string, que são a "len, lower e upper"')
+print('')
+
+t1 = str(input('Escreva algo e o programa dirá quantos caracteres possuem seu texto: '))
+print(len(t1))
+print('')
+
+t2 = str(input('Escreva um texto em maísculo e o programa converterá para minúsculo:  '))
+print(t2)
+print(t2.lower())
+print('')
+
+t3 = str(input('Escreva um texto em minúsculo e o programa converterá para maísculo:  '))
+print(t3)
+print(t3.upper())
