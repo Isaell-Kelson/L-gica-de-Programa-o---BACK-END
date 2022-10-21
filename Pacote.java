e85e3679577d9facd1d3c22a011e413b34103a46
Isaell Kelson <113737085+Isaell-Kelson@users.noreply.github.com>
2022-10-21 10:25:29 -0300

    Add files via upload

---
+++ Pacote.java
@@ -0,0 +1,16 @@
+import br.com.backflix.face.*;
+import br.com.backflix.videos.*;
+
+public class Pacote {
+    public static void main(String[] args) throws Exception {
+        Filme filme = new Filme();
+        
+        Serie serie = new Serie();
+
+        pause pausar = new pause();
+
+        play player = new play();
+
+    
+    }
+}
